1fe489d7c1e7c58e8f7a86980cd55ff3abd290db
MoOsman5 <mohamed1592000gmail.com>
2023-06-26 23:25:19 +0200

    add components to app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container } from "react-bootstrap";
+import MoviesList from "./components/MoviesList";
+import NavBar from "./components/header/NavBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import MovieDetails from "./components/MovieDetails";
 
 function App() {
+  const [pageCount, setpageCount] = useState(0);
+  const [Data, setData] = useState([]);
+  const getAllMovies = async () => {
+    const res = await axios.get(
+      "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=d06effa40fabf4254a63bbe87a2ca2fd"
+    );
+    setData(res.data.results);
+    if (res.data.total_pages > 500) {
+      setpageCount(500);
+    } else {
+      setpageCount(res.data.total_pages);
+    }
+  };
+
+  useEffect(() => {
+    getAllMovies();
+  }, []);
+
+  const search = async (word) => {
+    if (word === "") {
+      getAllMovies();
+    } else {
+      const res = await axios.get(
+        `https://api.themoviedb.org/3/search/movie?sort_by=popularity.desc&api_key=d06effa40fabf4254a63bbe87a2ca2fd&query=${word}`
+      );
+      setData(res.data.results);
+      if (res.data.total_pages > 500) {
+        setpageCount(500);
+      } else {
+        setpageCount(res.data.total_pages);
+      }
+    }
+  };
+  const getPage = async (page) => {
+    const res = await axios.get(
+      `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=d06effa40fabf4254a63bbe87a2ca2fd&page=${page}`
+    );
+    setData(res.data.results);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar search={search} />
+      <Container>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MoviesList Data={Data} getPage={getPage} pageCount={pageCount}/>}/>
+            <Route path="/movie/:id" element={<MovieDetails/>} />
+          </Routes>
+        </BrowserRouter>
+      </Container>
     </div>
   );
 }
